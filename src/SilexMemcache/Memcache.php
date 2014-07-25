0f6405dafbb32ccc1276a1784bdbbaa23753a7fa
billotei <ateilhet@gmail.com>
2014-07-25 16:24:16 +0200

    initial commit

---
+++ src/SilexMemcache/Memcache.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Memcache;
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Memcache.php';
+
+class Memcache extends \Memcache {}
